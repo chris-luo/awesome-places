9e5117b8f780360ff3d25e8429ed8895ddaf410c
Chris Luo <chris.ying.luo@gmail.com>
2018-07-26 23:04:09 -0700

    added input and button

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    placeName: '',
+    places: []
+  }
+
+  placeNameChangeHandler = val => {
+    this.setState({
+      placeName: val
+    })
+  }
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      }
+    });
+  }
+
   render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <Text key={i}>{place}</Text>
+    ));
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.inputContainer} >
+          <TextInput
+          style={styles.placeInput}
+          placeholder="An awesome place"
+          value={this.state.placeName} 
+          onChangeText={this.placeNameChangeHandler} />
+          <Button 
+          styles={styles.placeButton} 
+          title="Add"
+          onPress={this.placeSubmitHandler} />
+        </View>
+        <View>
+          {placesOutput}
+        </View>
       </View>
     );
   }
@@ -16,8 +54,22 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 26,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  inputContainer: {
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
 });
