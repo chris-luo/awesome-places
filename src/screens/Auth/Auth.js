d78c131e30ed4356c98ed8f0a0cbf478629a7575
Chris Luo <chris.ying.luo@gmail.com>
2018-08-01 21:00:45 -0700

    added background image

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet, ImageBackground } from 'react-native';
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import MainText from '../../components/UI/MainText/MainText';
+import backgroundImage from '../../assets/background.jpg';
 
 class AuthScreen extends Component {
     loginHandler = () => {
@@ -12,18 +13,20 @@ class AuthScreen extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <MainText>
-                    <HeadingText>Please Log In</HeadingText>
-                </MainText>
-                <Button title="Switch to Login" />
-                <View style={styles.inputContainer}>
-                    <DefaultInput placeholder="Your email" style={styles.input} />
-                    <DefaultInput placeholder="Password" style={styles.input} />
-                    <DefaultInput placeholder="Confirm Password" style={styles.input} />
+            <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+                <View style={styles.container}>
+                    <MainText>
+                        <HeadingText>Please Log In</HeadingText>
+                    </MainText>
+                    <Button title="Switch to Login" />
+                    <View style={styles.inputContainer}>
+                        <DefaultInput placeholder="Your email" style={styles.input} />
+                        <DefaultInput placeholder="Password" style={styles.input} />
+                        <DefaultInput placeholder="Confirm Password" style={styles.input} />
+                    </View>
+                    <Button title="Submit" onPress={this.loginHandler}/>
                 </View>
-                <Button title="Submit" onPress={this.loginHandler}/>
-            </View>
+            </ImageBackground>
         );
     }
 }
@@ -33,6 +36,10 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    backgroundImage: {
+        width: "100%",
+        flex: 1
     },
     inputContainer: {
         width: "80%"
