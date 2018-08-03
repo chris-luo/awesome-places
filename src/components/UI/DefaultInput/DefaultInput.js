c1f10c4f211501982e571c38280c2f1748197831
Chris Luo <chris.ying.luo@gmail.com>
2018-08-02 22:47:45 -0700

    added responsive auth screen

--- src/components/UI/DefaultInput/DefaultInput.js
+++ src/components/UI/DefaultInput/DefaultInput.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { TextInput, StyleSheet } from 'react-native';
 
 const defaultInput = props => (
-    <TextInput 
-    underlineColorAndroid="transparent"
-     {...props} 
-     style={[styles.input, props.style]} />
+    <TextInput
+        underlineColorAndroid="transparent"
+        {...props}
+        style={[styles.input, props.style]} />
 );
 
 const styles = StyleSheet.create({
@@ -14,7 +14,8 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: "#eee",
         padding: 5,
-        margin: 8
+        marginTop: 8,
+        marginBottom: 8
     }
 })
 
