bffc28eb613468803197b227f0f05059886ee83c
Chris Luo <chris.ying.luo@gmail.com>
2018-08-07 22:38:39 -0700

    store place on firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,6 +1,32 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
+    return dispatch => {
+        fetch("https://us-central1-awesome-places-1533493318776.cloudfunctions.net/storeImage", {
+            method: "POST",
+            body: JSON.stringify({
+                image: image.base64
+            })
+        })
+            .catch(err => console.log(err))
+            .then(res => res.json())
+            .then(parsedRes => {
+                const placeData = {
+                    name: placeName,
+                    location: location,
+                    image: parsedRes.imageUrl
+                }
+                return fetch("https://awesome-places-1533493318776.firebaseio.com/places.json", {
+                    method: "POST",
+                    body: JSON.stringify(placeData)
+                })
+            })
+            .catch(err => console.log(err))
+            .then(res => res.json())
+            .then(parsedRes => {
+                console.log(parsedRes)
+            });
+    }
     return {
         type: ADD_PLACE,
         placeName: placeName,
