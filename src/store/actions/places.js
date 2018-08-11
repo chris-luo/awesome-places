dac730392be552cc80c5b2b8411f7e3570715156
Chris Luo <chris.ying.luo@gmail.com>
2018-08-10 21:51:26 -0700

    check res for ok

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -32,7 +32,13 @@ export const addPlace = (placeName, location, image) => {
                 alert("Something went wrong, please try again!");
                 dispatch(uiStopLoading());
             })
-            .then(res => res.json())
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw (new Error());
+                }
+            })
             .then(parsedRes => {
                 const placeData = {
                     name: placeName,
@@ -44,7 +50,13 @@ export const addPlace = (placeName, location, image) => {
                     body: JSON.stringify(placeData)
                 })
             })
-            .then(res => res.json())
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw (new Error());
+                }
+            })
             .then(parsedRes => {
                 console.log(parsedRes);
                 dispatch(uiStopLoading());
@@ -79,7 +91,13 @@ export const getPlaces = () => {
             .then(token => {
                 return fetch("https://awesome-places-1533493318776.firebaseio.com/places.json?auth=" + token)
             })
-            .then(res => res.json())
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw (new Error());
+                }
+            })
             .then(parsedRes => {
                 const places = [];
                 for (let key in parsedRes) {
@@ -119,7 +137,13 @@ export const deletePlace = (key) => {
                     method: "DELETE"
                 })
             })
-            .then(res => res.json())
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw (new Error());
+                }
+            })
             .then(parsedRes => {
                 console.log(parsedRes);
             })
